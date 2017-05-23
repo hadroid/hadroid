bee9c48a168c7975615c5ec4f5d98311aa28fb40
Krzysztof Nowak <k.nowak@cern.ch>
2017-05-23 20:27:51 +0200

    PyPI deployment fixes
    Signed-off-by: Krzysztof Nowak <k.nowak@cern.ch>

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv3)',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
